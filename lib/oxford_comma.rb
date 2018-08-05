7c1d63b5703ec6bfd481b0a4096f7b95b8b917c7
Michael Roose <mjroose@gmail.com>
2018-08-05 17:16:07 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,5 +4,9 @@ def oxford_comma(array)
     array.join()
   when 2
     array.join(" and ")
+  else
+    new_array = array
+    new_array[-1] = "and #{new_array[-1]}"
+    new_array.join(", ")
   end
 end
